5cc13984ea9d5dc6fafd662fe6cec6fe2b2be8e2
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-27 03:06:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function BoardMember(name, homeState, training) {
   this.approve = "You can do that!";
   this.doCharity = "I like to help people.";
   this.releasePressStatement = "You will see great things from Scuber.";
-  this.sayHi = `Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.`;
+  this.sayHi = `Hi, my name is ${name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
 }
